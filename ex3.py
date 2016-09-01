8ee1f6dd70ab0ecd20eb20415bcddcc48a0b7fc1
Olus202 <aleksandrabacik@gmail.com>
2016-09-01 15:21:35 +0200

    First four exercises - printing, comments, numbers and math.

---
+++ ex3.py
@@ -0,0 +1,42 @@
+# Printing of the string.
+print "I will now count my chickens:"
+
+# Printing of the string and the result of math calculation,
+# division first, then addition.
+print "Hens", 25 + 30 / 6
+
+# Printing of the string and the result of math calculation,
+# multiplication first, then modulo, then subtraction.
+print "Roosters", 100 - 25 * 3 % 4
+
+# Printing of the string.
+print "Now I will count the eggs:"
+
+# Printing of the math calculation,
+# division first, then modulo, then addition and subtraction.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Printing of the string with question.
+print "Is is true that 3 + 2 < 5 - 7?"
+
+# Printing of the equation, boolean value.
+print 3 + 2 < 5 - 7
+
+# Printing of the string and the result of the math calculation.
+print "What is 3 + 2?", 3 + 2
+
+# Printing of the string and the result of the math calculation.
+print "What is 5 - 7?", 5 - 7
+
+# Printing of the string.
+print "Oh, that's why it's False."
+
+# Printing of the string.
+print "How about some more."
+
+# Printing of the string and the result of the equation.
+print "Is it grather?", 5 > -2
+# Printing of the string and the result of the equation.
+print "Is it grather or equal?", 5 >= -2
+# Printing of the string and the result of the equation.
+print "Is it less or equal?", 5 <= -2
