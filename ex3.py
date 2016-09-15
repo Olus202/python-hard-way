dc139a927239d77539e869deb1d489db46cb7dc6
Olus202 <aleksandrabacik@gmail.com>
2016-09-15 12:05:41 +0200

    Study drills update - changing the whole numbers into floating numbers

--- ex3.py
+++ ex3.py
@@ -3,30 +3,30 @@ print "I will now count my chickens:"
 
 # Printing of the string and the result of math calculation,
 # division first, then addition.
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 
 # Printing of the string and the result of math calculation,
 # multiplication first, then modulo, then subtraction.
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Printing of the string.
 print "Now I will count the eggs:"
 
 # Printing of the math calculation,
 # division first, then modulo, then addition and subtraction.
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Printing of the string with question.
 print "Is is true that 3 + 2 < 5 - 7?"
 
 # Printing of the equation, boolean value.
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # Printing of the string and the result of the math calculation.
-print "What is 3 + 2?", 3 + 2
+print "What is 3 + 2?", 3.0 + 2.0
 
 # Printing of the string and the result of the math calculation.
-print "What is 5 - 7?", 5 - 7
+print "What is 5 - 7?", 5.0 - 7.0
 
 # Printing of the string.
 print "Oh, that's why it's False."
@@ -35,8 +35,8 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Printing of the string and the result of the equation.
-print "Is it grather?", 5 > -2
+print "Is it grather?", 5.0 > -2.0
 # Printing of the string and the result of the equation.
-print "Is it grather or equal?", 5 >= -2
+print "Is it grather or equal?", 5.0 >= -2.0
 # Printing of the string and the result of the equation.
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.0 <= -2.0
