9bfa28e9a6240fd95247d05783bfd2c849bd2ee0
Olus202 <aleksandrabacik@gmail.com>
2016-09-19 15:20:58 +0200

    Exercise 9 - printing with three double-quotes end newline tag

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here is some new strange stuff, remember type it exactly.
+
+# Assignment the days to a variable 'days'
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Assignment the months to a variable 'months' - with special 'newline' tag.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Printing the days.
+print "Here are the days:", days
+# Printing the months, every month in new line.
+print "Here are the months:", months
+
+# Printing by special three double-quotes - all exactly like in code.
+print """
+There's somethnig going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even four lines if we want, or 5, or 6.
+"""
