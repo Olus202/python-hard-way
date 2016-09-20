830a1eeaa6efec1a47eb42a87ff6fe168e477c55
Olus202 <aleksandrabacik@gmail.com>
2016-09-20 11:09:28 +0200

    Exercise 13 - parameters, unpacking, varaibles (sys.argv)

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+# Study drills
+fourth = raw_input("Type three words:")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+print "There are yours three words:", fourth
