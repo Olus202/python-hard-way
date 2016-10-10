caa9d77e305797e7d06dfa57d9e91268b70ccd7e
Olus202 <aleksandrabacik@gmail.com>
2016-10-10 10:50:26 +0200

    Exercise 22 - memorization exercise

---
+++ ex22.py
@@ -0,0 +1,32 @@
+print # function which is printing something in the console
+# # octothrope, mesh, hash, pound - symbol of comment
+result, cars_not_driven # varialbes
+() # parathesis - used in math and functions
+* # symbol of multiplication
+/ # symbol of dividing
++ # symbol of addition for digits and concatenation for strings
+- # symbol of subtraction
+% # modulo - rest from dividing for digits and formating for strings
+, # in print it's concatenation
+"" # double-quotes for string - e.g. for sentences
+'' # quotes for string - e.g. for one-word string
+%d # formating of strings with digit
+%s # formating of strings with string
+% () # formating contents
+%r # formating of string without types - for debuging
+False # boolean value - logical 0
+= # assignment for variables
+'\n' # escape sequance - newline
+'\t' # escape sequance - tab
+""" """ # three double-quotes for multiline strings (docstrings)
+'\' # backslash for escape sequances and special characters
+raw_input() # function which is taking something form user
+from sys import argv # importing argv variable from 'sys' module
+argv # variable which is making empty variables
+open(file, type) # function which is opening a file in some type (r, w, wr)
+.read() # taking a file contents
+.truncate() # deleting of file contents
+.write() # writing text to a file
+.close() # closing a file
+*args = # argument which mean more arguments
+def cheese_and_crackers(argument) # definition of the functions
