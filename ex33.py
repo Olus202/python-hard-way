71f85810fe22a6452a741614f9f46a3245332a95
Olus202 <aleksandrabacik@gmail.com>
2016-10-12 14:51:03 +0200

    Exercice 33 - while loop in function

---
+++ ex33.py
@@ -0,0 +1,16 @@
+def function_while(var):
+    i = 0
+    numbers = []
+
+    while i < var:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+    print "The numbers: "
+
+    for num in numbers:
+        print num
