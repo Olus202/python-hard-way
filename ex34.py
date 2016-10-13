f6c4bbacf8777a2fe007353b423525db90a63cc3
Olus202 <aleksandrabacik@gmail.com>
2016-10-13 12:51:43 +0200

    Exercise 34 - list index

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+animals[1] # python
+animals[2] # peacock
+animals[0] # bear
+animals[3] # kangaroo
+animals[4] # whale
+animals[2] # peacock
+animals[5] # platypus
+animals[4] # whale
