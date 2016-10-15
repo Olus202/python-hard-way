99cd82f016b8d2b30385e72ac5fbe6ea079feb9c
Olus202 <aleksandrabacik@gmail.com>
2016-10-15 14:36:54 +0200

    Exercise 37 - keywords, data types, string escape sequences, string formats, operators

---
+++ ex37.py
@@ -0,0 +1,103 @@
+KEYWORDS
+and # operator of boolean logic, it's mean that all condition have to be True
+as # as in with-statements assigne sth to a variable (e.g. with open(file.txt, w) as f: ...)
+assert # it's checking if the condition is True (e.g in testing)
+break # it's breaking the loop
+class # word for defining a class
+continue # makieg that the loop is starting again
+def # word for defining a function (or method in class)
+del # deleting from dictionary
+elif # part of if-statement, used for many conditions
+else # part of if-statement, used for every diffrenet cases which are not define in conditions
+except # doing sth in the cases, when some exception happens
+exec # running a string as Python code
+finally # part of try-statement, do something like else
+for # for loop - do sth for every element of collection
+from # for importing element from module
+global # daclaration of global variable
+if # if-statement, for defining a first condition
+import # for importing modules
+in # operator in loops or if-statement; testing if sth is IN sth
+is # for testing of sth IS sth (like ==)
+lambda: # for creation of short function (sth = lambda y: y**2)
+not # operator of boolean logic - negation
+or # operator of boolean logic - some of the condition have to be True
+pass # it's doing nothing; empty block
+print # function which os printig string to screen
+raise # raise na exception when sth go wrong (raise ValuError("No") - short try/except)
+return # retunrning value from function (and exit it)
+try # part of try-statement, it's checking if sth can be done, if not then finnaly or except
+while # while loop, doing sth until it's True
+with # assignement a expresion to variable (with as)
+yield # generator; doing sth like loop but step by step
+
+DATA TYPES
+True # boolean value
+False # boolean value
+None # means nothing
+strings # texts (also character)
+numbers # intigers
+floats # decimal
+lists # lists of things (collections)
+dicts # dictionaries (key=value collections)
+
+STRING ESCAPE SEQUENCES
+'\\' # backslash
+'\'' # single-quote
+'\"' # doule-quote
+'\a' # bell/alarm
+'\b' # backspace
+'\f' # formfeed
+'\n' # newline
+'\t' # tab
+'\v' # vertical tab
+
+STRING FORMATS
+%d # numbers
+%i # intigers
+%o # octalnumber
+%u # unsigned decimal
+%x # hexadecimal lowercase
+%X # hexadecimal uppercase
+%e # exponential notation, lowercase e
+%E # exponential notation, uppercase E
+%f # floating
+%F # floating
+%g # either %f or %e, which is shorter
+%G # as %g, but uppercase
+%c # character
+%r # rep format (everything)
+%s # string
+%% # percent sign
+
+OPERATORS
++ # addition
+- # subtraction
+* # multiplication
+** # power of
+/ # division
+// # floor division
+% # modulus (or string formating)
+< # less than
+> # greather than
+<= # less than equal
+>= # greather than equal
+== # equal
+!= # not equal
+<> # not equal /not used/
+( ) # paranthesis
+[ ] # list brackets
+{ } # dict curly braces
+@ # at (decorators)
+, # coma
+: # colon
+. # dot
+= # assign equal
+; # semi-colon
++= # add and assign
+-= # subtract and assign
+*= # multiply and assign
+/= # divide and assign
+//= # floor divide and assign
+%= # modulus assign
+**= # power assign
