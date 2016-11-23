c63e69fb103ef1165a79514b1cf22dc238f4c1d4
Olus202 <aleksandrabacik@gmail.com>
2016-11-23 09:33:15 +0100

    Exercise 45 - game - to finished

---
+++ ex45_roomclasses.py
@@ -0,0 +1,14 @@
+class FirstRoom(object):
+
+    def __init__(self, live, demage):
+        self.live = live
+        self.demage = demage
+
+    def print_description(self):
+        print(description)
+
+    def choose_what_now(self):
+        print("Now you have to choose the way.")
+        choose = input("Right Room (1) or Left Room (2)? >> "")
+
+# I have to finishe that!!!
